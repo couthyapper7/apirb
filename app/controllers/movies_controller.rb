a2dea562a011b444cf5539911414c5b68d7b3817
couthyapper9 <couthyapper79@gmail.com>
2024-01-23 19:04:07 -0300

    api rest with less problems

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,18 +1,18 @@
 class MoviesController < ApplicationController
     def index
+        title = params[:t]
+        
+        movie = Movie.new(title)
+        movies = movie.search
 
-        movie = Movie.new({title: params[:t]})
-
-        if movie.valid?
-
-            movies = movie.search
+        if movies.all?(&:valid?)
 
             if movies.present? && movies.first.valid?
                 first_movie = movies.first
                 movie_data = {
-                title: first_movie.title,
-                director: first_movie.director,
-                year: first_movie.year
+                    title: first_movie.title,
+        
+                    year: first_movie.year
                 }
                 
                 render json: movie_data
@@ -20,7 +20,7 @@ class MoviesController < ApplicationController
                 render json: { error: 'Error: invalid data field' }, status: :bad_request
             end
         else
-        render json: { error: 'Error: invalid movie tittle' }, status: :bad_request
+        render json: { error: 'Error: invalid movie title' }, status: :bad_request
         end
     end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,21 +1,16 @@
 class Movie < ApplicationRecord
-    class Movie
-        include ActiveModel::Validations
+    include ActiveModel::Validations
 
-        attr_accessor :title
+    attr_accessor :title
 
-        validates :title, presence: true, format: { with: /\A[a-zA-Z0-9\s]+\z/, message: "only allows letters, numbers and spaces" }
+    validates :title, presence: true, format: { with: /\A[a-zA-Z0-9\s]+\z/, message: "only allows letters, numbers and spaces" }
 
-        def initialize(title)
-            @title = title
-        end
-    
-        def search
-            encoded_title = URI.encode(@title)
-        
-            client = Omdb::Api::Client.new(api_key: '9e5ea815')
-        
-            client.search(search: encoded_title)
-        end
+    def initialize(title)
+        @title = title
+    end
+
+    def search
+        client = Omdb::Api::Client.new(api_key: '9e5ea815')
+        client.search(title)
     end
 end
